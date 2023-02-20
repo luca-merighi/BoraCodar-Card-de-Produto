fd0bfff697e0a1cac4bc14079e24bd354058e0ed
Luca Merighi <73050019+luca-merighi@users.noreply.github.com>
2023-02-20 16:29:49 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'light-purple': '#D9CDF7',
+        'text-purple': '#271A45'
+      }
+    },
+  },
+  plugins: [],
+}
